884bc844a9d85addce9f3d2d9da9d825ff589952
Kamiichi <paperone430@gmail.com>
2022-05-21 11:33:43 +0900

    some tools

--- nand2tetris/projects/04/fill/Fill.asm
+++ nand2tetris/projects/04/fill/Fill.asm
@@ -6,9 +6,37 @@
 // Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
+// the screen should remain fully black as long as the key is pressed.
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+(LOOP)
+  @6000
+  D=M
+  @SCREEN
+  D; JGT
+  @LOOP
+  0; JMP
+
+(SCREEN)
+  @6000
+  D=M
+  @LOOP
+  D; JEQ
+  @4000
+  M=1
+  @4001
+  M=1
+  @4002
+  M=1
+  @4003
+  M=1
+  @4004
+  M=1
+  @4005
+  M=1
+  @SCREEN
+  0; JMP
